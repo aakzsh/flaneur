655e235cbab31e4dc41cfbcce72c3b3efdd0e80f
shruti <shrutigupta5555@gmail.com>
2022-06-19 11:54:12 +0530

    dashboard polaroid component

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -10,8 +10,21 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  final colors = [
+    0xffD6EAFC,
+    0xffFCD6D6,
+    0xffAEF8A7,
+    0xffDD98FE,
+    0xffFFCEA0,
+    0xffFFE9AF
+  ];
+
+  bool checkedValue = false;
+
   @override
   Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: AppColors().bgclr,
       body: SafeArea(
@@ -51,10 +64,65 @@ class _DashboardState extends State<Dashboard> {
                     shape: CircleBorder(),
                   )
                 ],
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              polaroid(w)
             ],
           ),
         )),
+      ),
+    );
+  }
+
+  polaroid(w) {
+    return Container(
+      height: 270,
+      width: 0.45 * w,
+      color: Color(colors[0]),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Container(
+              height: 150,
+              width: 0.35 * w,
+              color: Colors.white,
+              child: Center(
+                child: Text("gotta clean",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontStyle: FontStyle.italic, color: Colors.black)),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 24.0),
+                child: Text(
+                  "18 June 2022",
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic, color: Colors.black),
+                ),
+              ),
+              Checkbox(
+                activeColor: Colors.black,
+                side: BorderSide(
+                  color: Colors.black, //your desire colour here
+                  width: 1.5,
+                ),
+                value: checkedValue,
+                onChanged: (newValue) {
+                  setState(() {
+                    checkedValue = newValue!;
+                  });
+                },
+              )
+            ],
+          )
+        ],
       ),
     );
   }
