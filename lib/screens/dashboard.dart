1618acdc00a542a1cfd3c858f004bbb824e30354
shruti <shrutigupta5555@gmail.com>
2022-06-19 10:59:15 +0530

    dashboard page ui

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flaneur/constants/colors.dart';
+import 'package:flaneur/screens/new.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -13,7 +14,48 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors().bgclr,
-      body: Center(child: Text("dashboard")),
+      body: SafeArea(
+        child: Container(
+            child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Icon(Icons.arrow_back_ios),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text("June 2022",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700, fontSize: 20)),
+                      ),
+                      Icon(Icons.arrow_forward_ios)
+                    ],
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => New()));
+                    },
+                    elevation: 2.0,
+                    fillColor: AppColors().accent,
+                    child: Icon(
+                      Icons.add,
+                      size: 25.0,
+                    ),
+                    padding: EdgeInsets.all(10.0),
+                    shape: CircleBorder(),
+                  )
+                ],
+              )
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
